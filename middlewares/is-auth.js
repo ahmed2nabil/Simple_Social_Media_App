530474c99a337bd17b07a617c3fc58c823e963c0
ahmed2nabil <ahmednabl992@gmail.com>
2021-08-29 14:14:04 +0200

    is_auth middleware'

--- middlewares/is-auth.js
+++ middlewares/is-auth.js
@@ -1,1 +1,23 @@
 const jwt = require("jsonwebtoken");
+
+const config = require('../utils/config');
+
+exports.verfiyToken = (req,res,next) => {
+    if(!req.headers['authorization']) {
+        const error = new Error('Not authenticated.');
+        error.statusCode = 401;
+        throw error;
+    }
+
+
+    var token = req.headers['authorization'].split(' ')[1];
+
+if(!token) 
+return res.status(403).json({msg: "No token provided"});
+jwt.verify(token,config.secret,function(err, decoded) {
+    if(err) return res.status(401).json({msg: "Unauthorized"});
+
+    req.userId = decoded.id;
+    next();
+})
+}
