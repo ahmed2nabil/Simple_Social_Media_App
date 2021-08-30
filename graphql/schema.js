52cd9d82d42646020133ad88c981828a7b396697
ahmed2nabil <ahmednabl992@gmail.com>
2021-08-30 14:22:15 +0200

    Add get Posts Query and Resolvers

--- graphql/schema.js
+++ graphql/schema.js
@@ -32,8 +32,13 @@ input PostInputData {
     content : String!
     imageUrl : String!
 }
+type PostData {
+    posts : [Post!]!
+    totalPosts : Int!
+}
 type RootQuery {
     login(email: String! , password: String!) : AuthData!
+    posts: PostData!
 }
 type RootMutat {
     createUser(userInput : UserInputData) : User!
